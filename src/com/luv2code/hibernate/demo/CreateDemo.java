cdeef141218077d48af446acf2d5e573aee0eae0
turan1996gs <turan1996gs@gmail.com>
2020-05-11 23:23:28 +0400

    first commit

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
     public static void main(String[] args) {
 
